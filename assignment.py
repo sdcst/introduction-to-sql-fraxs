c03717c306e1f65c358df9e8fb59ec674b74cf66
fraxs <90715521+fraxs@users.noreply.github.com>
2023-01-26 11:30:22 -0800

    done

--- assignment.py
+++ assignment.py
@@ -1,24 +1,75 @@
-#!python
+import sqlite3
+file = 'data.db'
+connection = sqlite3.connect(file)
+cursor = connection.cursor()
 
-"""
-Create a program that will store the database for a veterinary
-Each record needs to have the following information:
-id unique integer identifier
-pet name
-pet species (cat, bird, dog, etc)
-pet breed (persian, beagle, canary, etc)
-owner name
-owner phone number
-owner email
-owner balance (amount owing)
-date of first visit
+def createdata():
+    file = 'data.db'
+    connection = sqlite3.connect(file)
+    print(connection)
+    cursor = connection.cursor()
+    query ="""
+    create table if not exists customers (
+        id integer primary key autoincrement,
+        petname tinytext,
+        species tinytext,
+        name tinytext,
+        number tinytext,
+        email tinytext,
+        amountowed float,
+        firstvisit tinytext);
+        """
+    cursor.execute(query)
 
-create a program that will allow the user to:
-insert a new record into the database and save it automatically
-retrieve a record by their id and display all of the information
-retrieve a record by the email and display all of the information
-retrieve a record by phone number and display all of the information
+def addcustomer():
+    petname = input("Enter pet name: ")
+    species = input("Enter species of pet: ")
+    name = input("Enter name: ")
+    number = input("Enter number: ")
+    email = input("Enter email: ")
+    amountowed = float(input("Enter the amount owed: "))
+    firstvisit = input("Enter the date of first visit: ")
+    data = [petname,species,name,number,email,amountowed,firstvisit]
+    query = f"insert into customers (petname,species,name,number,email,amountowed,firstvisit) values ('{data[0]}','{data[1]}','{data[2]}','{data[3]}','{data[4]}',{data[5]},'{data[6]}');"
+    cursor = connection.cursor()  
+    cursor.execute(query)
+    connection.commit()
 
-You will need to create the table yourself. Consider what data types you will
-need to use.
-"""
+def readcustomerid():
+    name = input("Enter customers id: ")
+    query = f'select * from customers where id={name}'
+    cursor.execute(query)
+    result = cursor.fetchall()
+    print(result)
+
+def readcustomeremail():
+    name = input("Enter customers email: ")
+    query = f'select * from customers where email="{name}"'
+    cursor.execute(query)
+    result = cursor.fetchall()
+    print(result)
+
+def readcustomernumber():
+    name = input("Enter customers phone number: ")
+    query = f'select * from customers where number="{name}"'
+    cursor.execute(query)
+    result = cursor.fetchall()
+    print(result)
+for __name__ in "__main__":
+    createdata()
+    while True:
+        print("1. Add a new customer\n2. Retrieve customers information\n3. Exit")
+        x = input("Enter option: ")
+        if x == '1':
+            addcustomer()
+        if x == '2':
+            print("1. Search by ID\n2. Search by email\n3. Search by phone number")
+            y = input("Enter option: ")
+            if y == '1':
+                readcustomerid()
+            if y == '2':
+                readcustomeremail()
+            if y == '3':
+                readcustomernumber()
+        if x == '3':
+            exit()
--- ex6.getRecords.py
+++ ex6.getRecords.py
@@ -6,7 +6,7 @@ connection = sqlite3.connect(file)
 print(connection)
 
 cursor = connection.cursor()
-query = "select * from customers"
+query = "select name,cnum from customers where cnum > 10 order by cnum desc limit 2"
 """
 Note that instead of selecting ALL of the data, we can be more specific about what we select and
 where we select from:
